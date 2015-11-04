c2d8e0127ae8cbfc37f7bb82f33dffe93b105d77
rosabraatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-11-04 15:59:40 +0100

    Create CreateTables_Baseball.sql

---
+++ CreateTables_Baseball.sql
@@ -0,0 +1,62 @@
+CREATE TABLE Master (
+    playerID VARCHAR(10) PRIMARY KEY,
+    birthYear INTEGER,
+    birthMonth INTEGER,
+    birthDay INTEGER,
+    birthCountry VARCHAR(50),
+    birthState VARCHAR(30),
+    birthCity VARCHAR(50),
+    deathYear INTEGER,
+    deathMonth INTEGER,
+    deathDay INTEGER,
+    deathCountry VARCHAR(50),
+    deathState VARCHAR(20),
+    deathCity VARCHAR(50),
+    nameFirst VARCHAR(50),
+    nameLast VARCHAR(50),
+    nameGiven VARCHAR(255),
+    weight INTEGER,
+    height DOUBLE PRECISION,
+    bats VARCHAR(1),
+    throws VARCHAR(1),
+    debut DATE,
+    finalGame DATE,
+    retroID VARCHAR(9),
+    bbrefID VARCHAR(9)
+);
+
+CREATE TABLE HallOfFame (
+    playerID VARCHAR(10),
+    yearid INTEGER,
+    votedBy VARCHAR(64),
+    ballots INTEGER,
+    needed INTEGER,
+    votes INTEGER,
+    inducted VARCHAR(1),
+    category VARCHAR(20),
+    needed_note VARCHAR(25),
+    PRIMARY KEY ( playerID, yearid, votedBy )
+);
+
+CREATE TABLE Schools (
+    schoolID VARCHAR(15) PRIMARY KEY,
+    name_full VARCHAR(255),
+    city VARCHAR(55),
+    state VARCHAR(55),
+    country VARCHAR(55)
+);
+
+CREATE TABLE CollegePlaying (
+    playerID VARCHAR(9),
+    schoolID VARCHAR(15),
+    yearID INTEGER
+);
+
+CREATE TABLE Salaries (
+    yearID INTEGER,
+    teamID VARCHAR(3),
+    lgID VARCHAR(2),
+    playerID VARCHAR(9),
+    salary DOUBLE PRECISION,
+    PRIMARY KEY ( yearID, teamID, lgID, playerID )
+);
