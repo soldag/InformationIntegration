bbc39caa6395b1dbe5823e8ed5ebae3d9b5396f3
Rosa Braatz <rosa.braatz@student.hpi.uni-potsdam.de>
2015-12-11 14:41:08 +0100

    correct imdb parser

--- mappers/imdb_parser.py
+++ mappers/imdb_parser.py
@@ -1,4 +1,5 @@
 import psycopg2
+import datetime
 
 connection1 = psycopg2.connect(host='localhost',
 	                                   port=5432,
@@ -7,7 +8,7 @@ connection1 = psycopg2.connect(host='localhost',
 
 connection2 = psycopg2.connect(host='localhost',
                                        port=5432,
-                                       database='infint_integrated',
+                                       database='infinf_in_imdb',
                                        user='Rosa')
 
 cursor1 = connection1.cursor()
@@ -24,6 +25,8 @@ id_mapping_composers = {}
 #insert actors in person
 cursor1.execute('SELECT * FROM actors')
 print 'start persons'
+print datetime.datetime.now()
+
 for row in cursor1.fetchall():
 	if row[1] is not None:
 		splitted_name = row[1].split(', ')
@@ -39,15 +42,11 @@ for row in cursor1.fetchall():
 			first_name = splitted_name[len(splitted_name)-1]
 			second_splitted_name = splitted_name[0].split()
 			last_name = second_splitted_name[len(second_splitted_name)-1]
-			stage_name = ''
-			for y in range(0, len(second_splitted_name) -1):
-				stage_name += second_splitted_name[y]
-				if y != len(second_splitted_name) -1:
-					stage_name += ' '
+			stage_name = ' '.join(second_splitted_name[:-1])
 			stage_name = stage_name.replace('\'', '')
 		gender = row[2]
 		old_id = row[0]
-		new_id = "im_" + str(old_id)
+		new_id = "im_a_" + str(old_id)
 
 		id_mapping_actors[old_id] = new_id
 
@@ -55,12 +54,15 @@ for row in cursor1.fetchall():
 connection2.commit()
 
 print 'end persons'
+print datetime.datetime.now()
 
 #insert movies to movie
 
 cursor1.execute('SELECT * FROM movies')
 
 print 'start movies'
+print datetime.datetime.now()
+
 for row in cursor1.fetchall():
 	old_id = row[0]
 	title = row[1]
@@ -80,13 +82,17 @@ for row in cursor1.fetchall():
 
 	cursor2.execute('INSERT INTO movie(id,title,release_year,imdb_id) VALUES (%s,%s,%s,%s)', (new_id,title,release_year,imdb_id))
 connection2.commit()
+
 print 'end movies'
+print datetime.datetime.now()
 
 #write join table
 
 cursor1.execute('SELECT * FROM movies2actors')
 
 print 'start join table'
+print datetime.datetime.now()
+
 for row in cursor1.fetchall():
 	old_movie_id = row[0]
 	old_actor_id = row[1]
@@ -97,12 +103,16 @@ for row in cursor1.fetchall():
 
 	cursor2.execute('INSERT INTO person_movie(person_id, movie_id, job_id, role) VALUES (%s,%s,%s,%s)', (new_actor_id, new_movie_id, job_id, role))
 connection2.commit()
+
 print 'end join table'
+print datetime.datetime.now()
 
 #insert directors
 
 cursor1.execute('SELECT * FROM directors')
 print 'start directors'
+print datetime.datetime.now()
+
 for row in cursor1.fetchall():
 	if row[1] is not None:
 		splitted_name = row[1].split(', ')
@@ -118,14 +128,10 @@ for row in cursor1.fetchall():
 			first_name = splitted_name[len(splitted_name)-1]
 			second_splitted_name = splitted_name[0].split()
 			last_name = second_splitted_name[len(second_splitted_name)-1]
-			stage_name = ''
-			for y in range(0, len(second_splitted_name) -1):
-				stage_name += second_splitted_name[y]
-				if y != len(second_splitted_name) -1:
-					stage_name += ' '
+			stage_name = ' '.join(second_splitted_name[:-1])
 			stage_name = stage_name.replace('\'', '')
 		old_id = row[0]
-		new_id = "im_" + str(old_id)
+		new_id = "im_d_" + str(old_id)
 
 		id_mapping_directors[old_id] = new_id
 
@@ -133,28 +139,36 @@ for row in cursor1.fetchall():
 connection2.commit()
 
 print 'end directors'
+print datetime.datetime.now()
 
 #write join table for directors and movies
 
 cursor1.execute('SELECT * FROM movies2directors')
 
 print 'start join table directors'
+print datetime.datetime.now()
+
 for row in cursor1.fetchall():
 	old_movie_id = row[0]
 	old_director_id = row[1]
 	job_id = 3
 	comment = row[2]
+	role = ''
 	new_movie_id = id_mapping_movies[old_movie_id]
 	new_director_id = id_mapping_directors[old_director_id]
 
-	cursor2.execute('INSERT INTO person_movie(person_id, movie_id, job_id, comment) VALUES (%s,%s,%s,%s)', (new_director_id, new_movie_id, job_id, comment))
+	cursor2.execute('INSERT INTO person_movie(person_id, movie_id, job_id, comment, role) VALUES (%s,%s,%s,%s,%s)', (new_director_id, new_movie_id, job_id, comment, role))
 connection2.commit()
+
 print 'end join table directors'
+print datetime.datetime.now()
 
 #insert cinematgrs
 
 cursor1.execute('SELECT * FROM cinematgrs')
 print 'start cinematgrs'
+print datetime.datetime.now()
+
 for row in cursor1.fetchall():
 	if row[1] is not None:
 		splitted_name = row[1].split(', ')
@@ -170,14 +184,10 @@ for row in cursor1.fetchall():
 			first_name = splitted_name[len(splitted_name)-1]
 			second_splitted_name = splitted_name[0].split()
 			last_name = second_splitted_name[len(second_splitted_name)-1]
-			stage_name = ''
-			for y in range(0, len(second_splitted_name) -1):
-				stage_name += second_splitted_name[y]
-				if y != len(second_splitted_name) -1:
-					stage_name += ' '
+			stage_name = ' '.join(second_splitted_name[:-1])
 			stage_name = stage_name.replace('\'', '')
 		old_id = row[0]
-		new_id = "im_" + str(old_id)
+		new_id = "im_ci_" + str(old_id)
 
 		id_mapping_cinematgrs[old_id] = new_id
 
@@ -185,28 +195,34 @@ for row in cursor1.fetchall():
 connection2.commit()
 
 print 'end cinematgrs'
+print datetime.datetime.now()
 
 #write join table for cinematgrs and movies
 
 cursor1.execute('SELECT * FROM movies2cinematgrs')
 
 print 'start join table cinematgrs'
+print datetime.datetime.now()
+
 for row in cursor1.fetchall():
 	old_movie_id = row[0]
 	old_cinematgrs_id = row[1]
 	job_id = 4
 	comment = row[2]
+	role = ''
 	new_movie_id = id_mapping_movies[old_movie_id]
 	new_cinematgrs_id = id_mapping_cinematgrs[old_cinematgrs_id]
 
-	cursor2.execute('INSERT INTO person_movie(person_id, movie_id, job_id, comment) VALUES (%s,%s,%s,%s)', (new_cinematgrs_id, new_movie_id, job_id, comment))
+	cursor2.execute('INSERT INTO person_movie(person_id, movie_id, job_id, comment, role) VALUES (%s,%s,%s,%s,%s)', (new_cinematgrs_id, new_movie_id, job_id, comment, role))
 connection2.commit()
 print 'end join table cinematgrs'
+print datetime.datetime.now()
 
 #insert producers
 
 cursor1.execute('SELECT * FROM producers')
 print 'start producers'
+print datetime.datetime.now()
 for row in cursor1.fetchall():
 	if row[1] is not None:
 		splitted_name = row[1].split(', ')
@@ -222,14 +238,10 @@ for row in cursor1.fetchall():
 			first_name = splitted_name[len(splitted_name)-1]
 			second_splitted_name = splitted_name[0].split()
 			last_name = second_splitted_name[len(second_splitted_name)-1]
-			stage_name = ''
-			for y in range(0, len(second_splitted_name) -1):
-				stage_name += second_splitted_name[y]
-				if y != len(second_splitted_name) -1:
-					stage_name += ' '
+			stage_name = ' '.join(second_splitted_name[:-1])
 			stage_name = stage_name.replace('\'', '')
 		old_id = row[0]
-		new_id = "im_" + str(old_id)
+		new_id = "im_p_" + str(old_id)
 
 		id_mapping_producers[old_id] = new_id
 
@@ -237,28 +249,35 @@ for row in cursor1.fetchall():
 connection2.commit()
 
 print 'end producers'
+print datetime.datetime.now()
 
 #write join table for producers and movies
 
 cursor1.execute('SELECT * FROM movies2producers')
 
 print 'start join table producers'
+print datetime.datetime.now()
+
 for row in cursor1.fetchall():
 	old_movie_id = row[0]
 	old_producers_id = row[1]
 	job_id = 2
 	comment = row[2]
+	role = ''
 	new_movie_id = id_mapping_movies[old_movie_id]
 	new_producers_id = id_mapping_producers[old_producers_id]
 
-	cursor2.execute('INSERT INTO person_movie(person_id, movie_id, job_id, comment) VALUES (%s,%s,%s,%s)', (new_producers_id, new_movie_id, job_id, comment))
+	cursor2.execute('INSERT INTO person_movie(person_id, movie_id, job_id, comment, role) VALUES (%s,%s,%s,%s,%s)', (new_producers_id, new_movie_id, job_id, comment, role))
 connection2.commit()
 print 'end join table producers'
+print datetime.datetime.now()
 
 #insert writers
 
 cursor1.execute('SELECT * FROM writers')
 print 'start writers'
+print datetime.datetime.now()
+
 for row in cursor1.fetchall():
 	if row[1] is not None:
 		splitted_name = row[1].split(', ')
@@ -274,14 +293,10 @@ for row in cursor1.fetchall():
 			first_name = splitted_name[len(splitted_name)-1]
 			second_splitted_name = splitted_name[0].split()
 			last_name = second_splitted_name[len(second_splitted_name)-1]
-			stage_name = ''
-			for y in range(0, len(second_splitted_name) -1):
-				stage_name += second_splitted_name[y]
-				if y != len(second_splitted_name) -1:
-					stage_name += ' '
+			stage_name = ' '.join(second_splitted_name[:-1])
 			stage_name = stage_name.replace('\'', '')
 		old_id = row[0]
-		new_id = "im_" + str(old_id)
+		new_id = "im_w_" + str(old_id)
 
 		id_mapping_writers[old_id] = new_id
 
@@ -289,28 +304,35 @@ for row in cursor1.fetchall():
 connection2.commit()
 
 print 'end writers'
+print datetime.datetime.now()
 
 #write join table for writers and movies
 
 cursor1.execute('SELECT * FROM movies2writers')
 
 print 'start join table writers'
+print datetime.datetime.now()
+
 for row in cursor1.fetchall():
 	old_movie_id = row[0]
 	old_writers_id = row[1]
 	job_id = 6
 	comment = row[2]
+	role = ''
 	new_movie_id = id_mapping_movies[old_movie_id]
 	new_writers_id = id_mapping_writers[old_writers_id]
 
-	cursor2.execute('INSERT INTO person_movie(person_id, movie_id, job_id, comment) VALUES (%s,%s,%s,%s)', (new_writers_id, new_movie_id, job_id, comment))
+	cursor2.execute('INSERT INTO person_movie(person_id, movie_id, job_id, comment, role) VALUES (%s,%s,%s,%s,%s)', (new_writers_id, new_movie_id, job_id, comment,role))
 connection2.commit()
 print 'end join table writers'
+print datetime.datetime.now()
 
 #insert composers
 
 cursor1.execute('SELECT * FROM composers')
 print 'start composers'
+print datetime.datetime.now()
+
 for row in cursor1.fetchall():
 	if row[1] is not None:
 		splitted_name = row[1].split(', ')
@@ -326,14 +348,10 @@ for row in cursor1.fetchall():
 			first_name = splitted_name[len(splitted_name)-1]
 			second_splitted_name = splitted_name[0].split()
 			last_name = second_splitted_name[len(second_splitted_name)-1]
-			stage_name = ''
-			for y in range(0, len(second_splitted_name) -1):
-				stage_name += second_splitted_name[y]
-				if y != len(second_splitted_name) -1:
-					stage_name += ' '
+			stage_name = ' '.join(second_splitted_name[:-1])
 			stage_name = stage_name.replace('\'', '')
 		old_id = row[0]
-		new_id = "im_" + str(old_id)
+		new_id = "im_co_" + str(old_id)
 
 		id_mapping_composers[old_id] = new_id
 
@@ -341,22 +359,27 @@ for row in cursor1.fetchall():
 connection2.commit()
 
 print 'end composers'
+print datetime.datetime.now()
 
 #write join table for composers and movies
 
 cursor1.execute('SELECT * FROM movies2writers')
 
 print 'start join table writers'
+print datetime.datetime.now()
+
 for row in cursor1.fetchall():
 	old_movie_id = row[0]
 	old_composers_id = row[1]
 	job_id = 5
 	comment = row[2]
+	role = ''
 	new_movie_id = id_mapping_movies[old_movie_id]
 	new_composers_id = id_mapping_composers[old_composers_id]
 
-	cursor2.execute('INSERT INTO person_movie(person_id, movie_id, job_id, comment) VALUES (%s,%s,%s,%s)', (new_composers_id, new_movie_id, job_id, comment))
+	cursor2.execute('INSERT INTO person_movie(person_id, movie_id, job_id, comment, role) VALUES (%s,%s,%s,%s,%s)', (new_composers_id, new_movie_id, job_id, comment, role))
 connection2.commit()
 print 'end join table composers'
+print datetime.datetime.now()
 
 
